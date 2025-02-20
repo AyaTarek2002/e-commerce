9be0fab0a4b71f03636c8ddfdff44a8f653a9cb5
Aya Tarek Galal <ay3291666@gmail.com>
2025-02-20 16:01:28 +0200

    add project

---
+++ Api.js
@@ -0,0 +1,12 @@
+// var myhttp=new XMLHttpRequest();
+// var arr=[];
+// myhttp.open("GET","https://jsonplaceholder.typicode.com/posts");
+
+// myhttp.send();
+
+// myhttp.addEventListener("readystatechange",function(){
+//  if(myhttp.readyState==4 && myhttp.status==400){
+//     arr=myhttp.response;
+//     console.log(arr);
+//  }
+// })
